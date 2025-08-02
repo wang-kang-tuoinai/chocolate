0efdec118f7322c9c1c9d80d847019312b61515b
tuonai <3304520839@qq.com>
2025-08-02 18:37:36 +0800

    配置在线同步功能

---
+++ config-template.js
@@ -0,0 +1,9 @@
+// 配置模板文件 - 请复制到 script.js 中替换相应行
+// 在线数据存储配置
+const GITHUB_GIST_ID = 'eb841a74b948f2b873c150d0cc93a174'; // 你的Gist ID
+const GITHUB_TOKEN = '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'; // 你的访问令牌
+
+// 使用说明：
+// 1. 将上面的配置复制到 script.js 文件的第3-4行
+// 2. 确保你的Gist ID和访问令牌正确
+// 3. 保存文件后刷新网页即可使用在线同步功能 
